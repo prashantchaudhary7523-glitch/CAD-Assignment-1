da4e3630ddafd53b57c5da7dedf976a735a4930d
prashantchaudhary7523-glitch <prashantchaudhary7523@gmail.com>
2026-03-20 10:46:48 +0530

    Add files via upload
    this is user input program

---
+++ cad.py
@@ -0,0 +1,9 @@
+light=input("enter light:")
+if light=="red":
+    print("stop")
+elif light=="yellow":
+    print("get ready")
+elif light=="green":
+    print("go")
+else:
+    print("light is broken")
